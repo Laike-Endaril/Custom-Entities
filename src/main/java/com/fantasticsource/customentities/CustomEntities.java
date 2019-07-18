1a539303d4951dfe8e152a1c425545952e7563b0
Kyle Koder <silvertallon@gmail.com>
2019-07-17 23:10:07 -0700

    Technically now have a living entity that can be spawned from an egg

--- src/main/java/com/fantasticsource/customentities/CustomEntities.java
+++ src/main/java/com/fantasticsource/customentities/CustomEntities.java
@@ -1,14 +1,19 @@
 package com.fantasticsource.customentities;
 
+import net.minecraft.entity.EntityList;
+import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.common.config.Config;
 import net.minecraftforge.common.config.ConfigManager;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.event.entity.EntityJoinWorldEvent;
 import net.minecraftforge.fml.client.event.ConfigChangedEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.registry.EntityEntry;
 
-@Mod(modid = CustomEntities.MODID, name = CustomEntities.NAME, version = CustomEntities.VERSION)
+@Mod(modid = CustomEntities.MODID, name = CustomEntities.NAME, version = CustomEntities.VERSION, dependencies = "required-after:fantasticlib@[1.12.2.021,)")
 public class CustomEntities
 {
     public static final String MODID = "customentities";
@@ -25,5 +30,15 @@ public class CustomEntities
     public static void saveConfig(ConfigChangedEvent.OnConfigChangedEvent event)
     {
         if (event.getModID().equals(MODID)) ConfigManager.sync(MODID, Config.Type.INSTANCE);
+    }
+
+    @SubscribeEvent
+    public static void registerEntity(RegistryEvent.Register<EntityEntry> event)
+    {
+        String name = MODID + ":living";
+        EntityEntry entry = new EntityEntry(CustomLivingEntity.class, name);
+        entry.setRegistryName(name);
+        entry.setEgg(new EntityList.EntityEggInfo(new ResourceLocation(name), 0, 0xFFFFFFFF));
+        event.getRegistry().register(entry);
     }
 }
---
+++ src/main/java/com/fantasticsource/customentities/CustomLivingEntity.java
@@ -0,0 +1,12 @@
+package com.fantasticsource.customentities;
+
+import net.minecraft.entity.EntityLiving;
+import net.minecraft.world.World;
+
+public class CustomLivingEntity extends EntityLiving
+{
+    public CustomLivingEntity(World worldIn)
+    {
+        super(worldIn);
+    }
+}
