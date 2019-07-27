b016b6aebf25a1fd3a3d19a9b6bc7f2971aa7eb2
Kyle Koder <silvertallon@gmail.com>
2019-07-27 13:53:46 -0700

    Updated lib with experimental GUIRectTabView

--- src/main/java/com/fantasticsource/customentities/client/gui/LivingEntityGUI.java
+++ src/main/java/com/fantasticsource/customentities/client/gui/LivingEntityGUI.java
@@ -4,8 +4,8 @@ import com.fantasticsource.customentities.Network;
 import com.fantasticsource.mctools.gui.GUILeftClickEvent;
 import com.fantasticsource.mctools.gui.GUIScreen;
 import com.fantasticsource.mctools.gui.guielements.GUIElement;
+import com.fantasticsource.mctools.gui.guielements.rect.GUIGradientRect;
 import com.fantasticsource.mctools.gui.guielements.rect.GUITextRect;
-import com.fantasticsource.mctools.gui.guielements.rect.GradientRect;
 import com.fantasticsource.tools.datastructures.Color;
 import net.minecraft.client.Minecraft;
 import net.minecraft.util.math.Vec3d;
@@ -56,7 +56,7 @@ public class LivingEntityGUI extends GUIScreen
     {
         super.initGui();
 
-        guiElements.add(new GradientRect(this, 0, 0, 1, 1, BACKGROUND, BACKGROUND, BACKGROUND, BACKGROUND));
+        guiElements.add(new GUIGradientRect(this, 0, 0, 1, 1, BACKGROUND, BACKGROUND, BACKGROUND, BACKGROUND));
 
         createElement = new GUITextRect(this, 0, 0, 1, "Create", FOREGROUND, MOUSEOVER, ACTIVE);
         guiElements.add(createElement);
